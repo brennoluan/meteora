d2764d4545e593bdb9c81b288ba916f0b2d31dea
Brenno Luan <brennoluan8530@gmail.com>
2026-05-28 15:21:17 -0300

    chore: initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { CategoryModule } from './category/category.module';
+import { join } from 'path';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'), // nova config
+    }),
+    CategoryModule,
+  ],
+})
+export class AppModule {}
